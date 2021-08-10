33f548e123502becc4bfb134011a4fb3d050ed32
Evan Tanuwijaya <evan.tanuwijaya@ciputra.ac.id>
2021-08-10 21:44:03 +0700

    Ketinggalan user dan status pernikahan dan soal

---
+++ database/factories/StatusPernikahanFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\status_pernikahan;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StatusPernikahanFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = status_pernikahan::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}

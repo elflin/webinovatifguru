f4a7e3fc507a5c611a4c64cc43e6c20f952263c7
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-07-22 16:23:08 +0700

    feat: data survey on user detail

--- public/js/datatables-demo.js
+++ public/js/datatables-demo.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#dataTable').DataTable({
+    $('.table').DataTable({
         initComplete: function () {
             this.api()
                 .columns()
--- resources/views/admin/userShowHistory.blade.php
+++ resources/views/admin/userShowHistory.blade.php
@@ -151,10 +151,80 @@
     </div> --}}
         <div class="card shadow my-4">
             <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-purple">Data Survey</h6>
+            </div>
+            <div class="card-body">
+                <table class="table" id="dataTable1" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Perilaku</th>
+                            <th>Intensi</th>
+                            <th>Sikap</th>
+                            <th>Norma</th>
+                            <th>Efikasi</th>
+                            <th>Budaya</th>
+                            <th>Determinasi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($historyList as $hl)
+                            <tr>
+                                <td>{{ $hl['historyId'] }}</td>
+                                <td>
+                                    <a href="#" data-toggle="modal"
+                                        data-target="#historyPerilaku-{{ $hl['historyId'] }}">
+                                        Cek Detail
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#" data-toggle="modal"
+                                        data-target="#historyIntensi-{{ $hl['historyId'] }}">
+                                        Cek Detail
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#" data-toggle="modal"
+                                        data-target="#historySikap-{{ $hl['historyId'] }}">
+                                        Cek Detail
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#" data-toggle="modal"
+                                        data-target="#historyNorma-{{ $hl['historyId'] }}">
+                                        Cek Detail
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#" data-toggle="modal"
+                                        data-target="#historyEfikasi-{{ $hl['historyId'] }}">
+                                        Cek Detail
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#" data-toggle="modal"
+                                        data-target="#historyBudaya-{{ $hl['historyId'] }}">
+                                        Cek Detail
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="#" data-toggle="modal"
+                                        data-target="#historyDetermination-{{ $hl['historyId'] }}">
+                                        Cek Detail
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="card shadow my-4">
+            <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-purple">Data Pelatihan</h6>
             </div>
             <div class="card-body">
-                <table class="table" id="dataTable" width="100%" cellspacing="0">
+                <table class="table" id="dataTable2" width="100%" cellspacing="0">
                     <thead>
                         <tr>
                             <th>#</th>
@@ -216,7 +286,8 @@
                                     <td>{{ $progress->pelatihan->judul }}</td>
                                     <td>
                                         @if ($progress->path_submission)
-                                            <a target="_blank" href="{{ asset('submission') . '/' . $progress->path_submission }}">File</a>
+                                            <a target="_blank"
+                                                href="{{ asset('submission') . '/' . $progress->path_submission }}">File</a>
                                         @else
                                             -
                                         @endif
@@ -255,6 +326,225 @@
                                 <hr>
                             @endif
                         @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
+    @foreach ($historyList as $hl)
+        <div class="modal fade" id="historyPerilaku-{{ $hl['historyId'] }}" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">History ID: {{ $hl['historyId'] }} - Perilaku
+                            Inovatif Guru</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <table class="table">
+                            <tr>
+                                <th>Variabel</th>
+                                <th>Value</th>
+                                <th>Idea Generation</th>
+                                <th>Idea Promotion</th>
+                                <th>Idea Realization</th>
+                            </tr>
+                            <tr>
+                                <td>{{ $hl['variabel']['Perilaku Inovatif Guru'] }}</td>
+                                <td>{{ $hl['value']['Perilaku Inovatif Guru'] }}</td>
+                                <td>{{ $hl['dimensi']['Idea Generation'] }}</td>
+                                <td>{{ $hl['dimensi']['Idea Promotion'] }}</td>
+                                <td>{{ $hl['dimensi']['Idea Realization'] }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="historyIntensi-{{ $hl['historyId'] }}" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">History ID: {{ $hl['historyId'] }} - Perilaku
+                            Inovatif Guru</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <table class="table">
+                            <tr>
+                                <th>Variabel</th>
+                                <th>Value</th>
+                                <th>Intensi Berinovasi</th>
+                            </tr>
+                            <tr>
+                                <td>{{ $hl['variabel']['Intensi Berinovasi'] }}</td>
+                                <td>{{ $hl['value']['Intensi Berinovasi'] }}</td>
+                                <td>{{ $hl['dimensi']['Intensi Berinovasi'] }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="historySikap-{{ $hl['historyId'] }}" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">History ID: {{ $hl['historyId'] }} - Sikap
+                            Terhadap Inovasi</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <table class="table">
+                            <tr>
+                                <th>Variabel</th>
+                                <th>Value</th>
+                                <th>Family Expectations For Creativity</th>
+                                <th>Risk Of Tolerance</th>
+                                <th>Openness To External Knowledge</th>
+                            </tr>
+                            <tr>
+                                <td>{{ $hl['variabel']['Sikap Terhadap Inovasi'] }}</td>
+                                <td>{{ $hl['value']['Sikap Terhadap Inovasi'] }}</td>
+                                <td>{{ $hl['dimensi']['Family Expectations For Creativity'] }}</td>
+                                <td>{{ $hl['dimensi']['Risk Of Tolerance'] }}</td>
+                                <td>{{ $hl['dimensi']['Openness To External Knowledge'] }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="historyNorma-{{ $hl['historyId'] }}" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">History ID: {{ $hl['historyId'] }} - Norma
+                            Subyektif terhadap Kreativitas</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <table class="table">
+                            <tr>
+                                <th>Variabel</th>
+                                <th>Value</th>
+                                <th>Family Expectations For Creativity</th>
+                                <th>Leader Expectations For Creativity</th>
+                                <th>Customer Expectations For Creativity</th>
+                            </tr>
+                            <tr>
+                                <td>{{ $hl['variabel']['Norma Subyektif terhadap Kreativitas'] }}</td>
+                                <td>{{ $hl['value']['Norma Subyektif terhadap Kreativitas'] }}</td>
+                                <td>{{ $hl['dimensi']['Family Expectations For Creativity'] }}</td>
+                                <td>{{ $hl['dimensi']['Leader Expectations For Creativity'] }}</td>
+                                <td>{{ $hl['dimensi']['Customer Expectations For Creativity'] }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="historyEfikasi-{{ $hl['historyId'] }}" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">History ID: {{ $hl['historyId'] }} - Efikasi
+                            Berinovasi</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <table class="table">
+                            <tr>
+                                <th>Variabel</th>
+                                <th>Value</th>
+                                <th>Personal Assumptions/beliefs About Own Creativity</th>
+                                <th>Evidence-based Assessment Of Creative Self-efficacy</th>
+                            </tr>
+                            <tr>
+                                <td>{{ $hl['variabel']['Efikasi Berinovasi'] }}</td>
+                                <td>{{ $hl['value']['Efikasi Berinovasi'] }}</td>
+                                <td>{{ $hl['dimensi']['Personal Assumptions/beliefs About Own Creativity'] }}</td>
+                                <td>{{ $hl['dimensi']['Evidence-based Assessment Of Creative Self-efficacy'] }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="historyBudaya-{{ $hl['historyId'] }}" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">History ID: {{ $hl['historyId'] }} - Budaya Organisasi Berorientasi Pembelajaran</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <table class="table">
+                            <tr>
+                                <th>Variabel</th>
+                                <th>Value</th>
+                                <th>Commitment To Learning</th>
+                                <th>Shared Vision</th>
+                                <th>Open-mindedness</th>
+                                <th>Intraorganizational Knowledge Sharing</th>
+                            </tr>
+                            <tr>
+                                <td>{{ $hl['variabel']['Budaya Organisasi Berorientasi Pembelajaran'] }}</td>
+                                <td>{{ $hl['value']['Budaya Organisasi Berorientasi Pembelajaran'] }}</td>
+                                <td>{{ $hl['dimensi']['Commitment To Learning'] }}</td>
+                                <td>{{ $hl['dimensi']['Shared Vision'] }}</td>
+                                <td>{{ $hl['dimensi']['Open-mindedness'] }}</td>
+                                <td>{{ $hl['dimensi']['Intraorganizational Knowledge Sharing'] }}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="historyDetermination-{{ $hl['historyId'] }}" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">History ID: {{ $hl['historyId'] }} - Self-Determination</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <table class="table">
+                            <tr>
+                                <th>Variabel</th>
+                                <th>Value</th>
+                                <th>Intrinsic Motivation</th>
+                                <th>Identified Regulation</th>
+                            </tr>
+                            <tr>
+                                <td>{{ $hl['variabel']['Budaya Organisasi Berorientasi Pembelajaran'] }}</td>
+                                <td>{{ $hl['value']['Budaya Organisasi Berorientasi Pembelajaran'] }}</td>
+                                <td>{{ $hl['dimensi']['Intrinsic Motivation'] }}</td>
+                                <td>{{ $hl['dimensi']['Identified Regulation'] }}</td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
             </div>

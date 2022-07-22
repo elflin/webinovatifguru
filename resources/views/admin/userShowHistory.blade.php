402f1f11f09d98a0d4b0c05a7924f8bbfb507911
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-07-22 08:42:51 +0700

    fix: error on empty submission on user detail

--- resources/views/admin/userShowHistory.blade.php
+++ resources/views/admin/userShowHistory.blade.php
@@ -208,17 +208,19 @@
                             <th>Submission</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        @foreach ($user->progress_history->first()->progress as $progress)
-                            <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ $progress->pelatihan->judul }}</td>
-                                <td>
-                                    <a href="{{ asset('submission') . '/' . $progress->path_submission }}">File</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
+                    @if ($user->progress_history->count() > 0)
+                        <tbody>
+                            @foreach ($user->progress_history->first()->progress as $progress)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $progress->pelatihan->judul }}</td>
+                                    <td>
+                                        <a href="{{ asset('submission') . '/' . $progress->path_submission }}">File</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    @endif
                 </table>
             </div>
         </div>

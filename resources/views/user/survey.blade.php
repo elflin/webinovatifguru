7d5686f3a089da52405b132574cd20e6f248a03b
akristianto03 <63012597+akristianto03@users.noreply.github.com>
2022-08-01 12:14:12 +0700

    update img

--- resources/views/user/survey.blade.php
+++ resources/views/user/survey.blade.php
@@ -8,8 +8,8 @@
 <div class="pt-5"></div>
 <div class="pt-5"></div>
 <div class="w-100"
-    style="height: 30vh !important; background-image: url({{ asset('img/dummy/daftar.jpg') }}); background-size: cover; background-position: center;">
-    <div class="w-100 row align-items-center m-0 p-0" style="height: 30vh">
+    style="height: 30vh !important; background-image: url({{ asset('img/assets/zoom-pic.jpg') }}); background-size: cover; background-position: center;">
+    <div class="w-100 row align-items-center m-0 p-0" style="height: 30vh; background: rgba(0, 0, 0, 0.3) !important;">
         <div class="col justify-content-center text-center">
             <h1 class=" text-white font-weight-bold">
                 Survey
